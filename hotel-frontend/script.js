9bd62d3f4501067b10560bbae02b5d788f463c52
welandfr <welandfr@arcada.fi>
2026-04-16 14:24:12 +0300

    code challenge

--- hotel-frontend/script.js
+++ hotel-frontend/script.js
@@ -9,12 +9,52 @@ async function getRooms() {
     console.log(rooms);
     for (room of rooms) {
         document.getElementById("room-list").innerHTML += `
-            <li>
+            <option value="${room.id}">
                 ${room.room_number} -
                 ${room.room_type} -
                 ${room.price} €
-            </li>
+            </option>
         `;
     }
 }
 getRooms();
+
+
+async function getBookings() {
+    const res = await fetch(`${API_URL}/bookings`);
+    const bookings = await res.json();
+
+    console.log(bookings);
+    document.getElementById("bookings-list").innerHTML = "";
+    for (booking of bookings) {
+        document.getElementById("bookings-list").innerHTML += `
+            <li>
+                ${booking.id} - ${booking.datefrom}
+            </li>
+        `;
+    }
+}
+getBookings();
+
+async function saveBooking() {
+
+    const booking = {
+        room_id: document.getElementById("room-list").value,
+        guest_id: 1,
+        datefrom: document.getElementById("datefrom").value,
+        dateto: document.getElementById("dateto").value
+    }
+
+    const res = await fetch(`${API_URL}/bookings`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(booking)
+    });
+    const resData = await res.json();
+
+    console.log(resData);
+    getBookings();
+}
+
+
+document.getElementById('btn-save').addEventListener('click', saveBooking);
